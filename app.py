25b7483d828b8f132396cc4bb68b0146fe2ed36b
mtume <mtume2016@gmail.com>
2025-10-11 20:47:34 +0300

    changed to flask-chat-app

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 from markupsafe import escape
+from flask_socketio import SocketIO, send, emit
+
 
 
 app = Flask(__name__)
+
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
@@ -23,6 +27,12 @@ def show_user(user_id):
     # show the user with the given id, the id is an integer
     return f'User {user_id}'
 
+@app.route('/chat', methods=['GET', 'POST'])
+def message(name=None):
+    chats=[]
+    if request.method=='POST':
+        handlemessage({ "usermessage":request.form.get("msg")})
+    return render_template('message.html', name=name, chats=chats)
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
@@ -30,6 +40,17 @@ def signin():
         return show_user(request.form.get('email'))
     return render_template('signin.html')
 
+@socketio.on('message')
+def handlemessage(data):
+    print("Current chats: ", data)
+    emit("message", [data], boradcast=True)
+  
+
+@socketio.on('connect')
+def handleConnect(user):
+    print("User is connected: ", user)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, port=8080, debug=True, use_reloader=True)
+    #app.run(debug=True)
